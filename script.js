977615ff9c600292444ac7162c09646b487d6d8a
amytxc <tsaiax8@gmail.com>
2025-03-15 20:46:06 -0400

    memeory matching game

--- script.js
+++ script.js
@@ -19,11 +19,27 @@ let lockBoard = false;
     You must initialize the game board. You have been given a shuffleArray() function.
     This function should also reset the entire game board by making sure there's no HTML inside of the game-board div.
     Use the createCard() function to initialize each cardElement and add it to the gameBoard.
-
 */
 function initGame() {
     // Write your code here
+    // Clear the inner HTML of game-board div
+    let gameBoard = document.getElementById('game-board');
+    gameBoard.innerHTML = '';
 
+    // Create 16 cards total, 2 for each symbol 
+    // Create a new array that duplicates symbols twice
+    let allSymbols = symbols.concat(symbols);
+    
+    // Shuffle the new array so that the symbols are randomly ordered
+    shuffleArray(allSymbols);
+
+    // Iterate over the symbols array and create 2 cards for each symbol.
+    for (let i = 0; i < 16; i++) {
+        // Create a cardElement for the ith symbol
+        createCard(allSymbols[Math.floor(i)]);
+    }
+
+    // Reset board when user clicks restart button 
     document.getElementById('restart-btn').addEventListener('click', initGame);
 }
 
@@ -34,6 +50,26 @@ function initGame() {
 */
 function createCard(symbol) {
     // Write your code here
+
+    // Create div element with class card to represent the card in HTML
+    const cardElement = document.createElement('div');
+
+    // Add the 'card' class to the card using classList add()
+    cardElement.classList.add('card');
+
+    // Save the card's symbol within the element itself
+    cardElement.dataset.symbol = symbol;
+
+    // Add the event listener with the flipCard function
+    // Arrow function delays execution of the flipCard function
+    cardElement.addEventListener('click', () => flipCard(cardElement));
+
+    // Add the card to cards array
+    cards.push(cardElement);
+
+    // Add cardElement to gameboard HTML using appendChild
+    let gameBoard = document.getElementById('game-board')
+    gameBoard.appendChild(cardElement);
 }
 
 /*
@@ -45,9 +81,31 @@ function createCard(symbol) {
     want to check for a match using the checkForMatch() function. 
 */
 function flipCard(card) {
-    // If the board is supposed to be locked or you picked the same card you already picked
-    if (lockBoard || card === firstCard) return;
-    // Write your code here
+    // Write your code here    
+
+    // If board is locked or user clicked on the first card again or on a flipped card, do nothing
+    if (lockBoard || card === firstCard || card.classList.contains('flipped')) return;
+
+    // Add 'flipped' class and show the symbol
+    card.classList.add('flipped');
+
+    // Display symbol
+    card.textContent = card.dataset.symbol;
+
+
+    // If this is the first card picked, set firstCard
+    if (firstCard === null) {
+        firstCard = card;
+    } else {
+        // If first card is alrready picked, set secondCard
+        secondCard = card;
+
+        // Lock board to keep user from clicking on other cards
+        lockBoard = true;
+
+        // Check for match between first and second cards
+        checkForMatch();
+    }
 }
 
 /* 
@@ -57,7 +115,20 @@ function flipCard(card) {
 */
 function checkForMatch() {
     // Write your code here
-}
+    // Check if firstCard and secondCard match
+    // If their symbols match, take those card out of the game 
+    
+    if (firstCard === secondCard) {
+        unflipCards();
+    }
+
+    if (firstCard.dataset.symbol === secondCard.dataset.symbol) {
+        disableCards();
+    } else {
+        // If the cards don't match, then lock the board and unflip the cards
+        unflipCards();
+    }
+ }
 
 /* 
     Disable both of the cards by adding the "matched" class to them. The "matched" class will add CSS
@@ -66,6 +137,12 @@ function checkForMatch() {
 */
 function disableCards() {
     // Write your code here
+    // Add the matched class
+    firstCard.classList.add('matched');
+    secondCard.classList.add('matched');
+
+    // Reset board
+    resetBoard();
 }
  
 /* ---------------------  Everything under has already been done for you -------------------------- */
